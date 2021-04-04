18a1c60b68678d3a478982b5865301bb546313bf
arshamtabesh <arsham@tabesh.com>
2021-04-04 22:27:30 +0200

    implemented the users tab

--- app/src/main/java/com/arman/instagramclone/UsersTab.java
+++ app/src/main/java/com/arman/instagramclone/UsersTab.java
@@ -1,64 +1,89 @@
 package com.arman.instagramclone;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UsersTab#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class UsersTab extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ListView mListView;
+    private ArrayList mArrayList;
+    private ArrayAdapter mArrayAdapter;
 
     public UsersTab() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UsersTab.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static UsersTab newInstance(String param1, String param2) {
         UsersTab fragment = new UsersTab();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        mArrayList = new ArrayList();
+        mArrayAdapter = new ArrayAdapter(getContext(),
+                R.layout.list_view_item,mArrayList);
+        fetchAndDisplayAllUsers();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users_tab, container, false);
+        View mView = inflater.inflate(R.layout.fragment_users_tab, container, false);
+
+        mListView = mView.findViewById(R.id.listView);
+
+
+
+        return mView;
+    }
+
+    private void fetchAndDisplayAllUsers(){
+        ProgressDialog mProgressDialog = new ProgressDialog(getContext());
+        mProgressDialog.setMessage("Getting Users...");
+        mProgressDialog.show();
+        ParseQuery<ParseUser> mParseQuery  = ParseUser.getQuery();
+        mParseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
+        mParseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                mProgressDialog.dismiss();
+                if (e == null) {
+                    if (users.size() > 0){
+                        for (ParseUser user: users){
+                            mArrayList.add(user.getUsername());
+
+                        }
+                        mListView.setAdapter(mArrayAdapter);
+                        int i  =  mListView.getCount();
+                    }
+                }
+            }
+        });
+
+
     }
 }
